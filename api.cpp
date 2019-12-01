48b56c0a75fd6b4e2384ee393d2498e905ff5f95
Catminusminus <resonafolma@gmail.com>
2019-12-02 01:09:43 +0900

    implement type-hinted path params
    you can use typed path params in request handlers

--- api.cpp
+++ api.cpp
@@ -1,4 +1,5 @@
 #include "blazingapi.hpp"
+#include <sstream>
 
 int main() {
   using namespace blazingapi;
@@ -8,13 +9,36 @@ int main() {
     return R"({"hello":"world"})";
   };
   server.Get(R"(/yo/(\d+))") = [] (const blazingapi::PathParams& path_params) {
-    const auto number1 = path_params.at(0);
+    const auto number1_str = path_params.at(0);
+    auto number1_stream = std::istringstream(number1_str);
+    int number1;
+    number1_stream >> number1;
     using json = nlohmann::json;
     json j;
     j["number1"] = number1;
     return j;
   };
-  server.Get(R"(/numbers/(\d+)/numbers/(\d+))") = [] (const blazingapi::PathParams& path_params, const blazingapi::QueryParams& query_params){
+  server.Get(R"(/ho/(\d+))") = [] (int path_param) {
+    using json = nlohmann::json;
+    json j;
+    j["number"] = path_param;
+    return j;
+  };
+  server.Get(R"(/hoho/(\d+)/(\d+))") = [] (int path_param_int, float path_param_float) {
+    using json = nlohmann::json;
+    json j;
+    j["int_num"] = path_param_int;
+    j["float_num"] = path_param_float;
+    return j;
+  };
+  server.Get(R"(/yoho/(\d+))") = [] (int path_param, const QueryParams& query_params) {
+    using json = nlohmann::json;
+    json j;
+    j["p"] = (query_params.find("p")->second);
+    j["number"] = path_param;
+    return j;
+  };
+  server.Get(R"(/numbers/(\d+)/numbers/(\d+))") = [] (const PathParams& path_params, const QueryParams& query_params){
     const auto number1 = path_params.at(0);
     using json = nlohmann::json;
     json j;
@@ -25,6 +49,10 @@ int main() {
   server.run(1234);
 }
 
-// curl -v "http://localhost:1234/hi"
-// curl -v "http://localhost:1234/yo/5"
-// curl -v "http://localhost:1234/numbers/5/numbers/6?q=1234&p=2345&r=3456"
+// curl -v "http://localhost:1234/hi" => {"hello":"world"}
+// curl -v "http://localhost:1234/yo/5" => {"number1":5}
+// curl -v "http://localhost:1234/ho/5" => {"number":5}
+// curl -v "http://localhost:1234/hoho/5/10" => {"float_num":10.0,"int_num":5}
+// curl -v "http://localhost:1234/yoho/5?p=aaaa" => {"number":5,"p":"aaaa"}
+// curl -v "http://localhost:1234/numbers/5/numbers/6?q=aaaa&p=bbbb&r=cccc" => {"number1":"5","p":"bbbb"}
+// curl -v "http://localhost:1234/nonexist" => {"detail":"Not Found"}
--- blazingapi.hpp
+++ blazingapi.hpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <functional>
 #include <vector>
+#include <sstream>
+#include <utility>
 
 namespace blazingapi {
   using QueryParams = httplib::Params;
@@ -37,6 +39,11 @@ namespace blazingapi {
         return x;
       }
     };
+
+    template <class F, class T, T... Seq>
+    auto make_applied_tuple(F func, std::integer_sequence<T, Seq...>) {
+      return std::make_tuple(func(Seq)...);
+    }
   }
   class BlazingAPI{
     httplib::Server internal_server;
@@ -53,26 +60,55 @@ namespace blazingapi {
       template <class F>
       auto operator=(F&& f) {
         auto return_json = [&f](const httplib::Request &req, httplib::Response &res) {
-          //bad
           if constexpr(detail::function_traits<decltype(detail::make_function(f))>::nargs == 0) {
             res.set_content(detail::to_string(std::invoke(f)), "application/json");
           } else if constexpr(detail::function_traits<decltype(detail::make_function(f))>::nargs == 1) {
             using arg = typename detail::function_traits<decltype(detail::make_function(f))>::template arg_t<0>;
-            if constexpr(!std::is_same_v<arg, QueryParams>) {
+            if constexpr(std::is_same_v<std::decay_t<arg>, PathParams>) {
               PathParams path_params_;
               for (std::size_t i = 1, n = req.matches.size(); i < n; ++i)
                 path_params_.emplace_back(req.matches.str(i));
               res.set_content(detail::to_string(std::invoke(f, path_params_)), "application/json");
+            } else if constexpr(std::is_same_v<std::decay_t<arg>, QueryParams>) {
+              res.set_content([&f, &req]{ return detail::to_string(std::invoke(f, req.params)); }(), "application/json");
             } else {
-              [&]{
-                res.set_content(detail::to_string(std::invoke(f, req.params)), "application/json");
-              }();
+              const auto path_param = req.matches.str(1);
+              auto path_stream = std::istringstream(path_param);
+              std::decay_t<arg> path_arg;
+              path_stream >> path_arg;
+              res.set_content([&f, &path_arg]{ return detail::to_string(std::invoke(f, path_arg)); }(), "application/json");
+            }
+          } else if constexpr(std::is_same_v<std::decay_t<typename detail::function_traits<decltype(detail::make_function(f))>::template arg_t<detail::function_traits<decltype(detail::make_function(f))>::nargs - 1>>, QueryParams>) {
+            using arg = typename detail::function_traits<decltype(detail::make_function(f))>::template arg_t<0>;
+            if constexpr(std::is_same_v<std::decay_t<arg>, PathParams>) {
+              PathParams path_params;
+              for (std::size_t i = 1, n = req.matches.size(); i < n; ++i)
+                path_params.emplace_back(req.matches.str(i));
+              res.set_content(detail::to_string(std::invoke(f, path_params, req.params)), "application/json");
+            } else {
+              const auto func = [&](std::size_t i){
+                const auto path_param = req.matches.str(i + 1);
+                auto path_stream = std::istringstream(path_param);
+                std::decay_t<arg> path_arg;
+                path_stream >> path_arg;
+                return path_arg;
+              };
+              constexpr auto number_of_path_params = detail::function_traits<decltype(detail::make_function(f))>::nargs - 1;
+              auto applied_tuple = detail::make_applied_tuple(func, std::make_index_sequence<number_of_path_params>());
+              res.set_content(detail::to_string(std::apply(f, std::tuple_cat(applied_tuple, std::make_tuple(req.params)))), "application/json");
             }
           } else {
-            PathParams path_params;
-            for (std::size_t i = 1, n = req.matches.size(); i < n; ++i)
-              path_params.emplace_back(req.matches.str(i));
-            res.set_content(detail::to_string(std::invoke(f, path_params, req.params)), "application/json");
+            using arg = typename detail::function_traits<decltype(detail::make_function(f))>::template arg_t<0>;
+            const auto func = [&](std::size_t i){
+              const auto path_param = req.matches.str(i + 1);
+              auto path_stream = std::istringstream(path_param);
+              std::decay_t<arg> path_arg;
+              path_stream >> path_arg;
+              return path_arg;
+            };
+            constexpr auto number_of_path_params = detail::function_traits<decltype(detail::make_function(f))>::nargs;
+            auto applied_tuple = detail::make_applied_tuple(func, std::make_index_sequence<number_of_path_params>());
+            res.set_content(detail::to_string(std::apply(f, applied_tuple)), "application/json");
           }
         };
         std::invoke(method, sever, url.c_str(), std::move(return_json));
@@ -93,7 +129,6 @@ namespace blazingapi {
           spdlog::info("Request params: {0} {1}", key, value);
         for (std::size_t i = 0, n = req.matches.size(); i < n; ++i)
           spdlog::info("Request matches: {}", req.matches.str(i));
-        spdlog::info("number: {}", req.matches.size() - 1 + req.params.size());
       });
       return BlazingAPIInternal(internal_server, &httplib::Server::Get, url);
     }
