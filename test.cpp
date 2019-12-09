0cd10f78c2ce890a0642966dda49975a7d122ec1
Catminusminus <resonafolma@gmail.com>
2019-12-10 02:25:18 +0900

    update test.cpp
    add a test which will fail

--- test.cpp
+++ test.cpp
@@ -4,11 +4,18 @@
 int main() {
   using namespace blazingapi;
   using namespace boost::ut;
-  "get(/hi) will be ok"_test = [] {
+  "get(/hi) test"_test = [] {
     auto server = BlazingAPI();
     server.Get("/hi") = [] { return R"({"hello":"world"})"; };
     auto client = TestClient(server, 8080);
-    auto response = client.Get("/hi");
-    expect(response->status == 200_i);
+    "ok"_test = [&] {
+      auto response = client.Get("/hi");
+      expect(response->status == 200_i);
+    };
+    // this test will be failed
+    "ng"_test = [&] {
+      auto response = client.Get("/yo");
+      expect(response->status == 400_i);
+    };
   };
 }
